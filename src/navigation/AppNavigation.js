5f7ff5e2337cacfcea0a1f762456bf03df66f900
Aleksandr Berezin <besrezen90@mail.ru>
2020-03-01 16:28:48 +0300

    add drawer

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -3,9 +3,12 @@ import { createAppContainer } from 'react-navigation'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
+import { createDrawerNavigator } from 'react-navigation-drawer'
 import { Platform } from 'react-native'
 import { MainScreen } from '../screens/MainScreen'
 import { PostScreen } from '../screens/PostScreen'
+import { AboutScreen } from '../screens/AboutScreen'
+import { CreateScreen } from '../screens/CreateScreen'
 import { BookedScreen } from '../screens/BookedScreen'
 import { THEME } from '../theme'
 import { Ionicons } from '@expo/vector-icons'
@@ -72,4 +75,24 @@ const BottomNavigator =
 				},
 		  })
 
-export const AppNavigation = createAppContainer(BottomNavigator)
+const AboutNavigator = createStackNavigator(
+	{
+		About: AboutScreen,
+	},
+	navOptions
+)
+
+const CreateNavigator = createStackNavigator(
+	{
+		Create: CreateScreen,
+	},
+	navOptions
+)
+
+const MainNavigator = createDrawerNavigator({
+	PostTabs: BottomNavigator,
+	About: AboutNavigator,
+	Create: CreateNavigator,
+})
+
+export const AppNavigation = createAppContainer(MainNavigator)
