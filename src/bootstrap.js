6314fa5af2af4dbda84964cc2d4bd4e943fc3e90
Aleksandr Berezin <besrezen90@mail.ru>
2020-03-01 13:54:47 +0300

    add take photo icon

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,8 +1,8 @@
 import * as Font from 'expo-font'
 
 export async function bootstrap() {
-  await Font.loadAsync({
-    'open-bold': require('../assets/fonts/OpenSans-Bold.ttf'),
-    'open-regular': require('../assets/fonts/OpenSans-Regular.ttf')
-  })
+    await Font.loadAsync({
+        'open-bold': require('../assets/fonts/OpenSans-Bold.ttf'),
+        'open-regular': require('../assets/fonts/OpenSans-Regular.ttf')
+    })
 }
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { View, Text, StyleSheet, Button, FlatList } from 'react-native'
+import { HeaderButtons, Item, HeaderButton } from "react-navigation-header-buttons"
 import { Post } from "../components/Post"
 import { DATA } from "../data"
+import { AppHeaderIcon } from "../components/AppHeaderIcon"
 
 export const MainScreen = ({ navigation }) => {
     const onOpenHandler = (post) => {
@@ -16,7 +18,10 @@ export const MainScreen = ({ navigation }) => {
 }
 
 MainScreen.navigationOptions = {
-    headerTitle: "My blog"
+    headerTitle: "My blog",
+    headerRight: (<HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
+        <Item title="Take photo" iconName="ios-camera" onPress={() => console.log("Press photo")} />
+    </HeaderButtons>)
 }
 
 const styles = StyleSheet.create({
